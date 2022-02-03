45a28485656f6f995ae625d9d3a172c74f5ca010
Athena Giovanetti <athena.nha@gmail.com>
2022-02-03 13:47:03 -0300

    ate o 10

--- Fundamentos/JS e Lógica da Pŕogramação/dia-1-javascript-primeiros-passos/exercicios-4.1.js
+++ Fundamentos/JS e Lógica da Pŕogramação/dia-1-javascript-primeiros-passos/exercicios-4.1.js
@@ -65,3 +65,26 @@ switch (piece.toLowerCase()){
         console.log("erro");   
         break;
     }
+     //7
+let nota = 50;
+if(nota >= 90){
+    console.log("A");
+} 
+else if(nota >= 80){
+    console.log("B");
+}
+else if(nota >= 70){
+    console.log("C");
+} 
+else if(nota >= 60){
+    console.log("D");
+}
+else if(nota >= 50){
+    console.log("E");
+}
+else if(nota <50){
+    console.log("F");
+}
+else if(nota < 0 || nota > 100){
+    console.log("erro");
+}
